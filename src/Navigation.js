ed1643ab5889928d31eae65515dd597510f04750
danielw <daniel.hadi@wgs-id.com>
2023-11-09 22:06:31 +0700

    update: navigation

--- src/Navigation.js
+++ src/Navigation.js
@@ -10,6 +10,11 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import FeedDetailScreen from './screens/homeStack/FeedDetailScreen';
 import CreatePostScreen from './screens/homeStack/CreatePostScreen';
 import NotificationScreen from './screens/homeStack/NotificationScreen';
+import HomeSvgComponent from './assets/icons/HomeSvgComponent';
+import CraftSvgComponent from './assets/icons/CraftSvgComponent';
+import InventorySvgComponent from './assets/icons/InventorySvgComponent';
+import ProfileSvgComponent from './assets/icons/ProfileSvgComponent';
+import MarketplaceSvgComponent from './assets/icons/MarketplaceSvgComponent';
 
 // Stack
 
@@ -18,7 +23,7 @@ const HomeStack = createNativeStackNavigator();
 const HomeStackGroup = () => {
   return (
     <HomeStack.Navigator>
-      <HomeStack.Screen name="Feed" component={Feed} />
+      <HomeStack.Screen name="Home" component={Feed} />
       <HomeStack.Screen name="FeedDetailScreen" component={FeedDetailScreen} />
       <HomeStack.Screen name="CreatePostScreen" component={CreatePostScreen} />
       <HomeStack.Screen name="NotificationScreen" component={NotificationScreen} />
@@ -29,14 +34,40 @@ const HomeStackGroup = () => {
 // Bottom Tab
 const Tab = createBottomTabNavigator();
 
+const renderIcon = (name, focused) => {
+  let activeColor = focused ? '#131623' : '#9D9FAD';
+
+  switch (name) {
+    case 'Feed':
+      return <HomeSvgComponent width={23} height={22} style={{ color: activeColor }} />;
+    case 'Crafting':
+      return <CraftSvgComponent width={22} height={22} style={{ color: activeColor }} />;
+    case 'Marketplace':
+      return <MarketplaceSvgComponent width={22} height={22} style={{ color: activeColor }} />;
+    case 'Inventory':
+      return <InventorySvgComponent width={22} height={22} style={{ color: activeColor }} />;
+    case 'Profile':
+      return <ProfileSvgComponent width={22} height={22} style={{ color: activeColor }} />;
+  }
+};
+
 const TabGroup = () => {
   return (
-    <Tab.Navigator>
-      <Tab.Screen
-        name="HomeStackGroup"
-        component={HomeStackGroup}
-        options={{ headerShown: false }}
-      />
+    <Tab.Navigator
+      screenOptions={({ route, navigation }) => ({
+        tabBarIcon: ({ color, focused, size }) => {
+          const { name } = route;
+
+          return renderIcon(name, focused);
+        },
+        tabBarActiveTintColor: '#131623',
+        tabBarInactiveTintColor: '#9D9FAD',
+        tabBarLabelStyle: {
+          fontSize: 12,
+        },
+      })}
+    >
+      <Tab.Screen name="Feed" component={HomeStackGroup} options={{ headerShown: false }} />
       <Tab.Screen name="Crafting" component={Crafting} />
       <Tab.Screen name="Marketplace" component={Marketplace} />
       <Tab.Screen name="Inventory" component={Inventory} />
